2467dc44758acefe5bdc1ef69dfea7aaed5908e5
sen0rxol0 <sen0rxol0@baieflow.com>
2021-06-26 18:28:57 +0200

    Update edit book view, add invalid-feedback

--- crud_exam/resources/views/books/edit.blade.php
+++ crud_exam/resources/views/books/edit.blade.php
@@ -22,28 +22,28 @@
                 <label for="title">Titre</label>
                 <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title" value="{{old('title') ?? $book->title}}" required autofocus>
                 @error('title')
-                    <div class="alert alert-danger"><small>{{$message}}</small></div>
+                    <span class="invalid-feedback"><small>{{$message}}</small></span>
                 @enderror
             </div>
             <div class="mt-4">
                 <label for="author">Nom de l&apos;author</label>
                 <input class="form-control @error('author') is-invalid @enderror" type="text" name="author" id="author" value="{{old('author') ?? $book->author}}" required>
                 @error('author')
-                    <div class="alert alert-danger"><small>{{$message}}</small></div>
+                    <span class="invalid-feedback"><small>{{$message}}</small></span>
                 @enderror
             </div>
             <div class="mt-4">
                 <label for="comment">Avis</label>
                 <textarea class="form-control @error('comment') is-invalid @enderror" name="comment" id="comment" cols="20" rows="5" required>{{old('comment') ?? $book->comment}}</textarea>
                 @error('comment')
-                    <div class="alert alert-danger"><small>{{$message}}</small></div>
+                    <span class="invalid-feedback"><small>{{$message}}</small></span>
                 @enderror
             </div>
             <div class="mt-4">
                 <label for="rate">Note&lpar;0/20&rpar;</label>
                 <input class="form-control @error('rate') is-invalid @enderror" min="0" max="20" type="number" name="rate" id="rate" value="{{old('rate') ?? $book->rate}}" required>
                 @error('rate')
-                    <div class="alert alert-danger"><small>{{$message}}</small></div>
+                    <span class="invalid-feedback"><small>{{$message}}</small></span>
                 @enderror
             </div>
             <div class="my-4 d-flex justify-content-end">
